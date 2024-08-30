8d6321263394a09154549c18fac1fcc048191f09
advit2611 <advit214@gmail.com>
2024-08-30 02:10:02 -0600

    Adding type error handling

--- src/main.rs
+++ src/main.rs
@@ -7,16 +7,26 @@ fn main() {
 
     println!("Enter a number you want to guess:");
 
-    let mut guess= String::new();
+    let num = rand::thread_rng().gen_range(1..=100); 
 
-    let num = rand::thread_rng().gen_range(1..=1).to_string(); 
+    loop { 
+        let mut guess =  String::new();
 
-    loop {
-        guess.clear();
         io::stdin().read_line( &mut guess).expect("Expected a number");
-        if guess.trim() == num{
-            print!("Your guess({}) is correct!", guess.trim());
-            break;
+        if let Ok(guess) = guess.trim().parse::<i32>(){
+            if guess == num{
+                print!("Your Guess is correct! {guess}");
+                break
+            }
+            else if guess > num{
+                println!("Guess a little lower!")
+            }
+            else{
+                println!("Guess a little higher!")
+            }
+        }
+        else {
+            println!("Enter a numerical integer...")
         }
         println!("Wrong Guess ({}), try again! ", guess.trim())
     }
