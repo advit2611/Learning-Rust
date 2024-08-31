8d404c01040b5446917db05d1ce30ab48d2d925e
advit2611 <advit214@gmail.com>
2024-08-31 02:13:19 -0600

    Learnt about loops and functions

--- README.md
+++ README.md
@@ -4,3 +4,5 @@
   ```
 
   there needs to be `guess.clear()` statment to avoid any appending of future numbers to the buffer of `guess` after every iteration in the loop.
+
+- To label a loop you use `'<label-name>`, if you have loop in a loop.
--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,29 @@ use rand::Rng;
 fn main() {
     println!("I need a Software job!");
 
+    let farn = convet_celsius(98.0);
+
+    let celsius = convet_farnheit(farn);
+
+    println!("The temperature is {farn} degree celsius");
+    println!("The temperature is {celsius} degree farnheit");
+    
+    println!("The fibonacci for index 5 is {}", generate_fibonacci(5));
+
+    guessing_game();
+
+}
+
+fn convet_celsius (temperature : f32) -> f32{
+    return 5.0 * (temperature - 32.0)/9.0
+}
+
+fn convet_farnheit (temperature : f32) -> f32{
+    return (9.0 * (temperature )/5.0) + 32.0
+}
+
+fn guessing_game() {
+
     println!("Enter a number you want to guess:");
 
     let num = rand::thread_rng().gen_range(1..=100); 
@@ -15,7 +38,7 @@ fn main() {
         io::stdin().read_line( &mut guess).expect("Expected a number");
         if let Ok(guess) = guess.trim().parse::<i32>(){
             if guess == num{
-                print!("Your Guess is correct! {guess}");
+                println!("Your Guess is correct! {guess}");
                 break
             }
             else if guess > num{
@@ -29,5 +52,14 @@ fn main() {
             println!("Enter a numerical integer...")
         }
         println!("Wrong Guess ({}), try again! ", guess.trim())
+    }
+}
+
+fn generate_fibonacci(index:i64) -> i64{
+    if (index == 0) || (index == 1){
+        return 1
+    }
+    else{
+        return generate_fibonacci(index-1) + generate_fibonacci(index-2)
     }
 }
